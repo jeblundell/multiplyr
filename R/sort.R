c158284b9da92a9667bf43c2934c972e2f258d11
Jim Blundell <james.blundell@keble.ox.ac.uk>
2016-04-12 19:35:20 +0100

    Minor refactor of sort

--- R/sort.R
+++ R/sort.R
@@ -1,27 +1,30 @@
+.dots2names <- function (x, dots) {
+    as.vector (sapply (dots, function (x) { as.character (x$expr) }))
+}
+
+.sort.fastdf <- function (x, decreasing=FALSE, dots) {
+    #FIXME: parallel version
+    namelist <- .dots2names (x, dots)
+    cols <- match(namelist, attr(x, "colnames"))
+    bigmemory::mpermute (x[[1]], cols=cols)
+}
+
 #' @export
 sort.fastdf <- function (x, decreasing = FALSE, ...) {
     dots <- lazyeval::lazy_dots (...)
-    namelist <- as.vector (sapply (dots, function (x) { as.character (x$expr) }))
-    cols <- match(namelist, attr(x, "colnames"))
-    sortcol <- match(".sort", attr(x, "colnames"))
-
-    #FIXME: parallel version
-
-    #Is there an issue storing the sort order in the matrix itself?
-    x[[1]][,sortcol] <- bigmemory::morder (x[[1]], cols=cols, decreasing=decreasing)
-    bigmemory::mpermute (x[[1]], order = x[[1]][,sortcol])
+    .sort.fastdf (x, decreasing, dots)
 }
 
 #' @export
 group_by <- function (.data, ...) {
     dots <- lazyeval::lazy_dots (...)
-    namelist <- as.vector (sapply (dots, function (x) { as.character (x$expr) }))
+
+    namelist <- .dots2names (.data, dots)
     cols <- match(namelist, attr(.data, "colnames"))
     Gcol <- match(".sort", attr(.data, "colnames"))
 
+    .sort.fastdf (.data, decreasing=FALSE, dots)
     #FIXME: parallel version
-
-    bigmemory::mpermute (.data[[1]], cols=cols)
     sm1 <- bigmemory::sub.big.matrix (.data[[1]], firstRow=1, lastRow=nrow(.data[[1]])-1)
     sm2 <- bigmemory::sub.big.matrix (.data[[1]], firstRow=2, lastRow=nrow(.data[[1]]))
     if (length(cols) == 1) {
