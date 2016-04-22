2da5566ca35ad7a8e1c99d61ca17b515aed42086
Jim Blundell <james.blundell@keble.ox.ac.uk>
2016-04-22 01:27:13 +0100

    Unit tests for partitioning

---
+++ tests/testthat/test-partition.R
@@ -0,0 +1,85 @@
+context("partition")
+
+test_that("fastdf() partitions data evenly over 2 nodes by default", {
+    dat <- fastdf(x=1:100,
+                  f=factor(rep(c("f1", "f2"), each=50), levels=c("f1", "f2")),
+                  G=rep(c("A", "B", "C", "D"), each=25),
+                  alloc=1,
+                  cl=2)
+    res <- do.call (c, dat %>% cldo(nrow(.local[[1]])))
+    expect_equal (res, c(50, 50))
+    first <- do.call (c, dat %>% cldo(.first))
+    last <- do.call (c, dat %>% cldo(.last))
+    expect_equal (min(first), 1)
+    expect_equal (min(last), 50)
+    expect_equal (max(first), 51)
+    expect_equal (max(last), 100)
+
+    res <- sort (do.call (c, dat %>% cldo(.local[, "x"])))
+    expect_equal (res, 1:100)
+
+    res <- sort (do.call (c, dat %>% cldo(.local[, "f"])))
+    expect_equal (res, rep(c(1, 2), each=50))
+
+    res <- sort (do.call (c, dat %>% cldo(.local[, "G"])))
+    expect_equal (res, rep(c("A", "B", "C", "D"), each=25))
+
+    stopCluster (attr(dat, "cl"))
+})
+
+test_that("fastdf() partitions N=1 over 2 nodes sensibly", {
+    dat <- fastdf (x=1, y=1, cl=2)
+    res <- do.call (c, dat %>% cldo(.empty))
+    expect_equal (sort(res), c(FALSE, TRUE))
+    stopCluster (attr(dat, "cl"))
+})
+
+test_that("fastdf() partitions N=2 over 2 nodes sensibly", {
+    dat <- fastdf (x=1:2, y=1:2, cl=2)
+    res <- do.call (c, dat %>% cldo(nrow(.local[[1]])))
+    expect_equal (res, c(1, 1))
+    first <- do.call (c, dat %>% cldo(.first))
+    last <- do.call (c, dat %>% cldo(.last))
+    expect_equal (sort(first), c(1,2))
+    expect_equal (sort(last), c(1,2))
+    stopCluster (attr(dat, "cl"))
+})
+
+
+test_that("partition_group() can partition 2 groups over 2 nodes", {
+    dat <- fastdf (x=1:100, G=rep(c(1,2), length.out=100), cl=2) %>%
+        partition_group (G)
+    res <- do.call (c, dat %>% cldo(length(.grouped)))
+    expect_equal (res, c(1, 1))
+    res1 <- do.call (c, dat %>% cldo(sum(.grouped[[1]][, "G"]==1)))
+    res2 <- do.call (c, dat %>% cldo(sum(.grouped[[1]][, "G"]==2)))
+    expect_equal (sort(res1), c(0, 50))
+    expect_equal (sort(res2), c(0, 50))
+    expect_equal (res1, rev(res2))
+    res <- dat %>% cldo(.grouped[[1]][, "x"])
+    expect_equal (res[[1]], seq(1, 99, by=2))
+    expect_equal (res[[2]], seq(2, 100, by=2))
+    stopCluster (attr(dat, "cl"))
+})
+
+test_that("partition_group() with 1 group uses only 1 node", {
+    dat <- fastdf (x=1:100, G=rep(1, length.out=100), cl=2) %>%
+        partition_group (G)
+    res <- do.call (c, dat %>% cldo(.empty))
+    expect_equal (sort(res), c(FALSE, TRUE))
+    stopCluster (attr(dat, "cl"))
+})
+
+test_that("partition_group() with 3 groups partitions as 2,1 or 1,2", {
+    dat <- fastdf (x=1:99, G=rep(c(1,2,3), length.out=99), cl=2) %>%
+        partition_group (G)
+    res <- do.call (c, dat %>% cldo(length(.grouped)))
+    expect_equal (sort(res), c(1,2))
+
+    res <- dat %>% cldo(.groups)
+    expect_equal (sort(do.call(c, res)), c(1, 2, 3))
+    expect_equal (sort(c(length(res[[1]]),
+                         length(res[[2]]))), c(1,2))
+    stopCluster (attr(dat, "cl"))
+})
+
